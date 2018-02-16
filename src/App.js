fa9742799f685a544f405a6840e6d20ec4b139a3
SerafimPoch <serpochapinsky@gmail.com>
2018-02-16 18:45:46 +0200

    add main functional

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import { builder } from "./Search";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: "", dark: "" };
+    this.state = {
+      text: "",
+      content: ""
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.randomClick = this.randomClick.bind(this);
@@ -13,12 +16,29 @@ class App extends Component {
 
   async handleSubmit(e) {
     e.preventDefault();
+    const storeKeys = [];
     const build = await builder(this.state.text);
     const pages = build.query.pages;
     Object.keys(pages).map(e => {
-      console.log(pages[e].extract.split("  "));
+      storeKeys.push(pages[e]);
+    });
+    let dark = storeKeys.map((e, i) => {
+      return (
+        <div className="item">
+          <a
+            href={`https://en.wikipedia.org/?curid=${e.pageid}`}
+            target="_blank"
+          >
+            <p className="bold" key={i}>
+              {e.title}
+            </p>
+            <p key={i}>{e.extract}</p>
+          </a>
+        </div>
+      );
     });
     this.setState({
+      content: dark,
       text: ""
     });
   }
@@ -36,26 +56,27 @@ class App extends Component {
 
   render() {
     let text = this.state.text;
+    let content = this.state.content;
     return (
       <div className="container">
-        <div>
-          <button onClick={this.randomClick}>Random Page</button>
-        </div>
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              placeholder="Search..."
-              className="text"
-              value={text}
-              onChange={this.handleChange}
-            />
-          </form>
+        <div className="navBar">
+          <div>
+            <button onClick={this.randomClick}>Random Page</button>
+          </div>
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                placeholder="Search..."
+                className="text"
+                value={text}
+                onChange={this.handleChange}
+              />
+            </form>
+          </div>
         </div>
         <div className="page-container">
-          <div>
-            <a href="#">{this.state.dark}</a>
-          </div>
+          <div className="content-container">{content}</div>
         </div>
       </div>
     );
